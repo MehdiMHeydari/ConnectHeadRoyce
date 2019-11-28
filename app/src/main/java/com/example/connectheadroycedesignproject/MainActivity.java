0aeb501f02922e522119f6802b333a97469fab2a
mehdi <mehdih2022@headroyce.org>
2019-11-27 22:08:20 -0800

    buttons and login without code

--- app/src/main/java/com/example/connectheadroycedesignproject/MainActivity.java
+++ app/src/main/java/com/example/connectheadroycedesignproject/MainActivity.java
@@ -3,14 +3,39 @@ package com.example.connectheadroycedesignproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText User;
+    private EditText password;
+    private Button login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //hi
+        User = findViewById (R.id.user);
+        password = findViewById(R.id.password);
+        login = findViewById(R.id.login);
+
+    }
+
+    public class ButtonHandler implements View.OnClickListener {
+
+        //waits for click
+        @Override
+        public void onClick(View v) {
+
+
+
+
+        }
+
+
+
     }
 }
