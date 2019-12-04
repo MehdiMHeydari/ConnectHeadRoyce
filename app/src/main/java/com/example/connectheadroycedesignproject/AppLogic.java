b1091b1f802ab769461f9b1a9b27d50f470714c9
mehdi <mehdih2022@headroyce.org>
2019-12-04 10:28:18 -0800

    logic and beggining to get how to do get requests

--- app/src/main/java/com/example/connectheadroycedesignproject/AppLogic.java
+++ app/src/main/java/com/example/connectheadroycedesignproject/AppLogic.java
@@ -1,5 +1,26 @@
 package com.example.connectheadroycedesignproject;
 
 public class AppLogic {
+    private static String user;
+    private static String password;
+
+
+
+
+    public AppLogic{
+        user = MainActivity.UserData.GetUser();
+        password = MainActivity.UserData.GetPassword();
+
+    }
+
+    //public boolean checkuser{
+
+
+
+
+
+
+    //}
+
 }
 
--- app/src/main/java/com/example/connectheadroycedesignproject/MainActivity.java
+++ app/src/main/java/com/example/connectheadroycedesignproject/MainActivity.java
@@ -9,17 +9,21 @@ import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
-    private EditText User;
-    private EditText password;
+    public static EditText user;
+    public static EditText password;
     private Button login;
+    static String userdata2 = user.getText().toString();
+    static String passworddata2 = password.getText().toString();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        User = findViewById (R.id.user);
-        password = findViewById(R.id.password);
+        user = findViewById(R.id.user);
+       password = findViewById(R.id.password);
+
+
 
 
         int login = R.id.login;
@@ -32,7 +36,6 @@ public class MainActivity extends AppCompatActivity {
 
 
 
-
     public class Login implements View.OnClickListener {
 
 
@@ -40,18 +43,31 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public void onClick(View v) {
 
-        String userdata = User.getText().toString();
-        String passworddata = password.getText().toString();
-        System.out.println(userdata + " " +  passworddata);
-
-
-
-
+            String userdata = user.getText().toString();
+            String passworddata = password.getText().toString();
+            System.out.println(userdata + " " + passworddata);
 
 
         }
 
 
+    }
+
+    public static class UserData {
+
+        public static String GetUser(){
+            return userdata2;
+        }
+
+        public static String GetPassword(){
+            return passworddata2;
+        }
 
     }
+
+
+
+
 }
+
+
